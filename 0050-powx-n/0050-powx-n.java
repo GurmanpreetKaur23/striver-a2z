e93e54aba0f5cfa3276af52791dcd7d9ea468c7b
Gurmanpreet Kaur <119606083+GurmanpreetKaur23@users.noreply.github.com>
2024-12-23 22:03:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -2,9 +2,12 @@ class Solution {
     public double myPow(double x, int n) {
         // x-> base , n-> exp
 
-        if (n < 0) {
-            x = 1 / x;
-            n = -n ;
+        if (n == -2147483648) {
+           return  0.000000000465661;
+        }
+        if( n<0) {
+            x = 1/x ;
+            n = - n ;
         }
         if(n==0) return 1.0 ;
 
