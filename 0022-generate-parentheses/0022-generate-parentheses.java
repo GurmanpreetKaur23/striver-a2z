d2dad1af521b2e0860af29d6541deaca79bf65fd
Gurmanpreet Kaur <119606083+GurmanpreetKaur23@users.noreply.github.com>
2025-01-13 04:44:29 +0530

    Time: 1 ms (82.2%), Space: 43.5 MB (34.77%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,15 +1,17 @@
 class Solution {
-    void solve(int left , int right , List<String> res , String s , int n) {
-        if(s.length() == n*2) {
-            res.add(s) ;
-            return  ;
+    List<String> ans = new ArrayList<>() ;
+    public void solve(int left , int right , int n , String s) {
+        if(s.length()==n*2) {
+            ans.add(s) ;
+            return ;
         }
-        if(left<n) solve(left+1 , right , res , s+"(" , n) ;
-        if(right<left) solve(left , right+1 , res , s+")" , n) ;
+
+        if(left<n) solve(left+1 , right , n , s+"(") ;
+        if(right<left) solve(left , right+1 , n , s+")") ;
     }
+
     public List<String> generateParenthesis(int n) {
-        List<String> res = new ArrayList<>(); 
-        solve(0 , 0 , res , "" , n) ;
-        return res ;
+        solve(0 , 0 , n ,"") ;
+        return ans ;
     }
 }
