87606a419ac6949d080589db317c3b10cd69872c
Gurmanpreet Kaur <119606083+GurmanpreetKaur23@users.noreply.github.com>
2025-03-14 13:21:50 +0530

    Time: 4 ms (75.45%), Space: 45 MB (11.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,11 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] sArray = s.toCharArray() ;
-        char[] tArray = t.toCharArray() ;
+        // length check krage
+        if(s.length() != t.length()) return false ;
 
-        Arrays.sort(sArray) ;
-        Arrays.sort(tArray) ;
+        // convert to char array 
+        char[] s1 = s.toCharArray() ;
+        char[] t1 = t.toCharArray() ;
 
-        return Arrays.equals(sArray , tArray);
+        Arrays.sort(s1) ;
+        Arrays.sort(t1) ;
+
+        return Arrays.equals(s1 , t1);
     }
 }
