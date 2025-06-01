f21299bdf2cee7fe981b2a7b843aa2f5412311bc
Gurmanpreet Kaur <119606083+GurmanpreetKaur23@users.noreply.github.com>
2025-06-01 18:54:14 +0530

    Time: 2 ms (99.78%), Space: 49.9 MB (39.57%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,29 +1,28 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        if(grid.length == 0 || grid==null) return 0 ;
         int m = grid.length ;
         int n = grid[0].length ;
-
         int count = 0 ;
+
         for(int i=0 ; i<m ; i++) {
             for(int j=0 ; j<n ; j++) {
                 if(grid[i][j] == '1') {
                     dfs(grid , i , j) ;
-                    count++;
+                    count++ ;
                 }
             }
         }
         return count ;
     }
 
-    private void dfs(char[][] grid , int i , int j) {
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j] == '0') return ;
+    private void dfs(char[][] mat , int i , int j) {
+        if(i<0 || j<0 || i>=mat.length || j>=mat[0].length || mat[i][j] == '0') return;
 
-        grid[i][j] = '0' ;
+        mat[i][j] = '0' ;
 
-        dfs(grid , i+1 , j) ;
-        dfs(grid , i-1 , j) ;
-        dfs(grid , i , j+1) ;
-        dfs(grid , i , j-1) ;
+        dfs(mat , i+1 , j) ;
+        dfs(mat , i-1 , j) ;
+        dfs(mat , i , j+1) ;
+        dfs(mat , i , j-1) ;
     }
 }
