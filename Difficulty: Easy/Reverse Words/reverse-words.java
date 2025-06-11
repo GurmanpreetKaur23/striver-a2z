e370bba5180c8e043382a804782dcc7bcafd1245
Gurmanpreet Kaur <119606083+GurmanpreetKaur23@users.noreply.github.com>
2025-06-11 23:04:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse Words/reverse-words.java
+++ Difficulty: Easy/Reverse Words/reverse-words.java
@@ -4,11 +4,13 @@ class Solution {
         // Code here
         String[] words = s.split("\\s+") ;
         StringBuilder sb = new StringBuilder() ;
-        for(int i=words.length-1 ; i>=0 ; i--) {
+        
+        for(int i=words.length - 1 ; i>=0 ; i--) {
             sb.append(words[i]) ;
             
             if(i!=0) sb.append(" ") ;
         }
+        
         return sb.toString().trim();
     }
 }
