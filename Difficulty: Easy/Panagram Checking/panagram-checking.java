149024a362d70ca211b0110182da0f50413e6f20
Gurmanpreet Kaur <119606083+GurmanpreetKaur23@users.noreply.github.com>
2025-06-12 01:39:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Panagram Checking/panagram-checking.java
@@ -0,0 +1,21 @@
+// User function template for JAVA
+
+class Solution {
+    // Function to check if a string is Pangram or not.
+    public static boolean checkPangram(String s) {
+        // your code here
+        boolean[] seen = new boolean[26] ;
+        
+        for(char c : s.toCharArray()) {
+            if(Character.isLetter(c)) {
+                seen[Character.toLowerCase(c) - 'a'] = true ;
+            }
+        }
+        
+        for(boolean b : seen) {
+            if(!b) return false;
+        }
+        return true ;
+        
+    }
+}
