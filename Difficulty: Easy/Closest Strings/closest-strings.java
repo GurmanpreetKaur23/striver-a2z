f09420ddb1c4ef0e6295cef38827e7a6a3e496ab
Gurmanpreet Kaur <119606083+GurmanpreetKaur23@users.noreply.github.com>
2025-06-11 23:39:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Closest Strings/closest-strings.java
+++ Difficulty: Easy/Closest Strings/closest-strings.java
@@ -3,18 +3,15 @@
 class Solution {
     int shortestDistance(ArrayList<String> s, String word1, String word2) {
         // code here
-        int d1 = -1 ;
-        int d2 = -1 ;
-        int ans = Integer.MAX_VALUE ;
+        int d1 = -1 , d2 = -1 ;
+        int minValue = Integer.MAX_VALUE ;
         
         for(int i=0 ; i<s.size() ; i++) {
             if(s.get(i).equals(word1)) d1 = i ;
             if(s.get(i).equals(word2)) d2 = i ;
             
-            if(d1!=-1 && d2!=-1) {
-                ans = Math.min(ans , Math.abs(d1 - d2)) ;
-            }
+            if(d1 != -1 && d2 != -1) minValue = Math.min(minValue , Math.abs(d1 - d2)) ;
         }
-        return ans ;
+        return minValue ;
     }
 };
