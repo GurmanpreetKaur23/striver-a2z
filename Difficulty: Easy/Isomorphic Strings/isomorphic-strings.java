ffac1ab2c0f3ce980e0d056dbf46846d7cd24b30
Gurmanpreet Kaur <119606083+GurmanpreetKaur23@users.noreply.github.com>
2025-06-12 23:37:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Isomorphic Strings/isomorphic-strings.java
+++ Difficulty: Easy/Isomorphic Strings/isomorphic-strings.java
@@ -3,19 +3,18 @@ class Solution {
         // code here
         if(s1.length() != s2.length()) return false ;
         
-        int[] mapS1 = new int[256] ;
-        int[] mapS2 = new int[256] ;
+        int[] freq1 = new int[256] ;
+        int[] freq2 = new int[256] ;
         
         for(int i=0 ; i<s1.length() ; i++) {
             char c1 = s1.charAt(i) ;
-            char c2 = s2.charAt(i);
+            char c2 = s2.charAt(i) ;
             
-            if(mapS1[c1] != mapS2[c2]) return false ;
+            if(freq1[c1] != freq2[c2]) return false ;
             
-            mapS1[c1] = i + 1 ;
-            mapS2[c2] = i + 1 ;
-            
-        }
-        return true ;
+            freq1[c1] = i+1 ;
+            freq2[c2] = i+1 ;
+         }
+         return true ;
     }
 }
