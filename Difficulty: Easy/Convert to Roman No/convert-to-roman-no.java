c33411fae3d5fe241d6e1cc4c45c7d51d4dfb38b
Gurmanpreet Kaur <119606083+GurmanpreetKaur23@users.noreply.github.com>
2025-06-16 00:11:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Convert to Roman No/convert-to-roman-no.java
+++ Difficulty: Easy/Convert to Roman No/convert-to-roman-no.java
@@ -1,17 +1,17 @@
 class Solution {
     String convertToRoman(int n) {
         // code here
-        int[] values = {1000 , 900 , 500 , 400 , 100 , 90 , 50 , 40 , 10 , 9 , 5 , 4 , 1} ;
-        String[] symbols = {"M" , "CM" , "D" , "CD" , "C" , "XC" , "L" , "XL" , "X" , "IX", "V", "IV", "I"} ;
+        int[] values = {1000 , 900 , 500 , 400 , 100 , 90 , 50 , 40 , 10 , 9 , 5 ,4 , 1} ;
+        String[] symbols = {"M" , "CM" , "D" , "CD" , "C" , "XC" , "L" , "XL" , "X" , "IX" ,"V" , "IV" , "I"} ;
         
         StringBuilder sb = new StringBuilder() ;
-        
-        for(int i=0 ; i<values.length ; i++) {
-            while(n >= values[i]) {
+        for(int i = 0 ; i < values.length ; i++ ) {
+            while(n>=values[i]) {
                 sb.append(symbols[i]) ;
+                
                 n -= values[i] ;
             }
         }
-        return sb.toString() ;
+        return sb.toString();
     }
 }
