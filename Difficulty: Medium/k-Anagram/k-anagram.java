1a8ca84f4fd726078568d8827a7fe64071f5f106
Gurmanpreet Kaur <119606083+GurmanpreetKaur23@users.noreply.github.com>
2025-06-16 00:30:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/k-Anagram/k-anagram.java
+++ Difficulty: Medium/k-Anagram/k-anagram.java
@@ -6,20 +6,14 @@ class Solution {
         int[] freq1 = new int[26] ;
         int[] freq2 = new int[26] ;
         
-        for(char c : s1.toCharArray()) {
-            freq1[c - 'a']++;
-        }
+        for(char c : s1.toCharArray()) freq1[c - 'a']++ ;
+        for(char c : s2.toCharArray()) freq2[c - 'a']++ ;
         
-        for(char c : s2.toCharArray()) {
-            freq2[c - 'a']++ ;
-        }
         int changes = 0 ;
         for(int i=0 ; i<26 ; i++) {
-            if(freq1[i] >= freq2[i]) {
-                changes += freq1[i] - freq2[i] ;
-            }
+            if(freq1[i] >= freq2[i]) changes += freq1[i] - freq2[i] ;
         }
         
-        return changes<=k;
+        return changes<=k ;
     }
 }
