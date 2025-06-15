e8f8ee704eca5444c42f27662a0683f2dba94c44
Gurmanpreet Kaur <119606083+GurmanpreetKaur23@users.noreply.github.com>
2025-06-15 22:45:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse Words/reverse-words.java
+++ Difficulty: Easy/Reverse Words/reverse-words.java
@@ -2,10 +2,11 @@ class Solution {
     // Function to reverse words in a given string
     public String reverseWords(String s) {
         // Code here
-        String[] words = s.split("\\s+") ;
         StringBuilder sb = new StringBuilder() ;
         
-        for(int i=words.length - 1 ; i>=0 ; i--) {
+        String[] words = s.split("\\s+") ;
+        
+        for(int i=words.length-1 ; i>=0 ; i--) {
             sb.append(words[i]) ;
             
             if(i!=0) sb.append(" ") ;
