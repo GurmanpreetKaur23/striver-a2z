7f3b933c6265d471a5183833854adfc1fb4d278b
Gurmanpreet Kaur <119606083+GurmanpreetKaur23@users.noreply.github.com>
2025-06-16 02:57:31 +0530

    Time: 2 ms (98.63%), Space: 42.9 MB (82.81%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,8 +1,23 @@
 class Solution {
+    public boolean alphanum(char c) {
+        return (
+            (c>='A' && c<='Z') ||
+            (c>='a' && c<='z') ||
+            (c>='0' && c<='9')
+        ) ;
+    }
     public boolean isPalindrome(String s) {
-        String clean = s.toLowerCase().replaceAll("[^a-z0-9]","") ;
-        String rev = new StringBuilder(clean).reverse().toString() ;
+        int left = 0 ;
+        int right = s.length() - 1 ;
 
-        return clean.equals(rev) ;
+        while(left < right) {
+            while(left < right && !alphanum(s.charAt(left))) left++;
+            while(left < right && !alphanum(s.charAt(right))) right-- ;
+
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false ;
+            left++;
+            right--;
+        }
+        return true ;
     }
 }
