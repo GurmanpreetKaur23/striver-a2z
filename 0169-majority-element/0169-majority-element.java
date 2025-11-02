b2c121493eeeb2e89fdcc400cbed4055430da4ad
Gurmanpreet Kaur <119606083+GurmanpreetKaur23@users.noreply.github.com>
2025-11-02 23:09:58 +0530

    Time: 1931 ms (5.12%), Space: 55.2 MB (8.86%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,14 +1,15 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        // moore's voting algo
+        // int count = 0 ;
 
-        int count = 0 ;
-        int cand = 0;
-        for(int num : nums) {
-            if(count==0) cand = num ;
-            if(num == cand) count++ ;
-            else count-- ;
+        for(int i=0 ; i<nums.length ; i++) {
+            int count = 0 ;
+            for(int j=0 ; j<nums.length ; j++) {
+                if(nums[i] == nums[j]) count++ ;
+            }
+            if(count > (nums.length)/2) return nums[i] ;
+
         }
-        return cand ;
+        return -1 ;
     }
 }
