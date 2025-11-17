1c6a671ab08d4d51eaff5a521d2f55cff8968e17
Gurmanpreet Kaur <119606083+GurmanpreetKaur23@users.noreply.github.com>
2025-11-18 01:42:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxSubarraySum(int[] arr, int k) {
+        // Code here
+        int sum = 0; 
+        int max = 0;
+        
+        for(int i=0 ; i<k ; i++) sum += arr[i] ;
+        
+        max = sum ;
+        
+        for(int i=k ; i<arr.length ; i++) {
+            sum += arr[i] ;
+            // pichla kad do
+            sum -= arr[i-k] ;
+            max = Math.max(max , sum) ;
+        }
+        return max ;
+    }
+}
