3b046116263c32ae5fdb8d4ba161405a7baedd38
Gurmanpreet Kaur <119606083+GurmanpreetKaur23@users.noreply.github.com>
2025-12-07 01:51:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.java
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.java
@@ -1,22 +1,28 @@
 class Solution {
-    public ArrayList<Integer> helper(ArrayList<ArrayList<Integer>> adj , int node , boolean[] vis) {
-        vis[node] = true ;
-        ArrayList<Integer> ans = new ArrayList<>();
-        ans.add(node);
-        
-        for(int nei : adj.get(node)) {
-            if(!vis[nei]) {
-                ArrayList<Integer> subans = helper(adj , nei , vis) ;
-                ans.addAll(subans);
+    public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        Stack<Integer> st = new Stack<>() ;
+        ArrayList<Integer> ans = new ArrayList<>() ;
+        boolean[] vis = new boolean[adj.size()] ;
+        int start = 0 ;
+        // vis[start] = true ;
+        st.push(start) ;
+        while(!st.isEmpty()) {
+            int node = st.pop() ;
+            if(!vis[node]) {
+                vis[node] = true ;
+                ans.add(node) ;
+            
+            List<Integer> neighbors = adj.get(node) ;
+            for(int i=neighbors.size()-1 ; i>=0 ; i--) {
+                int nei = neighbors.get(i) ;
+                if(!vis[nei]) {
+                    st.push(nei) ;
+                    // ans.add(nei) ;
+                }
+            }
             }
         }
         return ans;
-    }
-    public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
-        // code here
-       
-        boolean[] vis = new boolean[adj.size()] ;
-         
-         return helper(adj, 0, vis);
     }
 }
